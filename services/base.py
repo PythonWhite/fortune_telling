7e2dc4bc34292a7d2cd641eadc14d3e9744a9c80
houkexin <h1372979604@163.com>
2019-10-15 23:14:23 +0800

    添加基础函数

--- services/base.py
+++ services/base.py
@@ -1,5 +1,6 @@
 from db import db, get_redis
 from abc import ABCMeta, abstractmethod
+from lib.constant import ERROR_DATA_FORMAT, ERROR_UNIQUE_DATA, ERROR_DATA_NOT_EXIST
 
 
 class ControlBase():
@@ -13,17 +14,48 @@ class ControlBase():
     def query_one(self, id):
         return self.model.query.get(id)
 
-    def query_list(self, **kwargs):
-        return self.model.query
-    
-    def add(self, **kwargs):
-        pass
+    def query_list(self, filters):
+        is_ok = self.check_data(filters)
+        if not is_ok:
+            return False, ERROR_DATA_FORMAT
+        return True, self.model.query.filter_by(**filters)
 
-    def batch_add(self, model_list):
-        pass
+    def add(self, parameter):
+        is_ok = self.check_data(parameter)
+        if not is_ok:
+            return False, ERROR_DATA_FORMAT
+        instance = self.model(**parameter)
+        self.db.session.add(instance)
+        self.db.session.commit()
+        return True, instance
 
-    def update(self, uniques, **kwargs):
-        pass
+    def batch_add(self, model_list, is_check=True):
+        if is_check:
+            for i in model_list:
+                if not self.check_data(i):
+                    return False, ERROR_DATA_FORMAT
+
+        self.db.session.execute(
+            self.model.__table__.insert(),
+            model_list
+        )
+        self.db.session.commit()
+        return True, ''
+
+    def update(self, uniques, data):
+        if not self.check_data(uniques):
+            return False, ERROR_UNIQUE_DATA
+
+        if not self.check_data(data):
+            return False, ERROR_DATA_FORMAT
+
+        instance = self.model.query.filter_by(**uniques).first()
+        if not instance:
+            return False, ERROR_DATA_NOT_EXIST
+        for i in data:
+            setattr(instance, i, data[i])
+        self.db.session.commit()
+        return True, instance
 
     def batch_update(self, uniques, data):
         pass
@@ -34,7 +66,18 @@ class ControlBase():
     def batch_delete(self, uniques):
         pass
 
+    def check_data(self, data):
+        if isinstance(data, str) and hasattr(self.model, data):
+            return True
+        elif isinstance(data, dict):
+            for i in data:
+                if not hasattr(self.model, i):
+                    return False
+
+            return True
+        else:
+            return False
+
     @abstractmethod
     def format_result(self, data):
         pass
-
